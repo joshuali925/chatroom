617c86542a48c402449c559cbd71295fa43bacaf
joshuali925 <joshuali925@gmail.com>
2019-05-18 17:37:18 -0400

    update style

--- client/client.js
+++ client/client.js
@@ -1,23 +1,30 @@
-// let user = prompt("Your name: ");
-let user = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
 let socket = io();
 
-document.title += ': ' + user;
 
+// let user = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+let user = prompt("Your name: ");
 socket.emit("join", user); // notify server
 
-socket.on("message", function(user, message) {
-    $('#messages').append($('<li class="list-group-item">').text(user + ": " + message));
+socket.on("duplicate", function() {
+    user = prompt("Duplicate name, try again: ");
+    socket.emit("join", user);
+    $("#input").attr('placeholder', user + ':');
+});
+
+
+socket.on("message", function(header, message) {
+    $('#messages').append($('<li class="list-group-item">').text(header + ": " + message));
     $('#message-list').scrollTop(1E9);
 });
 
-socket.on("notify", function(user, message, userlist) {
+socket.on("notify", function(message, userlist) {
     $('#messages').append($('<li class="list-group-item">').text(message));
     $('#message-list').scrollTop(1E9);
     $('#users').html(userlist.map(u => $('<li class="list-group-item">').text(u)));
 });
 
-$(document).ready(function(){
+$(document).ready(function() {
+    $("#input").attr('placeholder', user + ':');
     $("form").submit(function () {
         let message = $("#input").val();
         socket.emit("message", user, message);
--- server.js
+++ server.js
@@ -8,30 +8,39 @@ let sockets = {};
 
 app.use(express.static(path.join(__dirname, 'client')));
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket) {
+io.on('connection', function (socket) {
     let connected_user;
     socket.on("join", function (user) {
+        if (user in sockets) {
+            socket.emit("duplicate");
+            return;
+        }
+        let now = getTime();
         sockets[user] = socket;
         connected_user = user;
-        io.emit("notify", user, user + " entered the room.", Object.keys(sockets)); // notify clients
-        // console.log(user + " connected.");
+        io.emit("notify", now + user + " entered the room.", Object.keys(sockets)); // notify clients
     });
 
     socket.on("message", function (user, message) {
-        io.emit("message", user, message);
+        let now = getTime();
+        io.emit("message", now + user, message);
     });
 
-    socket.on("disconnect", function() {
+    socket.on("disconnect", function () {
+        let now = getTime();
         delete sockets[connected_user];
-        io.emit("notify", connected_user, connected_user + " left the room.", Object.keys(sockets)); // notify clients
-        // console.log(connected_user + " disconnected.");
+        io.emit("notify", now + connected_user + " left the room.", Object.keys(sockets)); // notify clients
     });
 });
 
-http.listen(8888, function(){
-    console.log('listening on port 8888');
+http.listen(8888, function () {
+    console.log('http://localhost:8888');
 });
+
+function getTime() {
+    return "(" + new Date().toLocaleTimeString() + ") ";
+}
